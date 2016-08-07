b42ec64c68fe30ce7662006d2b03e7c7ebea5182
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-07 13:49:26 +0200

    first test of checking that index page returns list of all countries

--- src/test/java/com/teamtreehouse/countries/controller/CountryControllerTest.java
+++ src/test/java/com/teamtreehouse/countries/controller/CountryControllerTest.java
@@ -1,23 +1,70 @@
 package com.teamtreehouse.countries.controller;
 
+import com.teamtreehouse.countries.data.CountryRepository;
+import com.teamtreehouse.countries.model.Country;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CountryControllerTest {
     private MockMvc mockMvc;
+    @InjectMocks
     private CountryController countryController;
+
+    @Mock
+    private CountryRepository countryRepository;
 
     @Before
     public void setUp() throws Exception {
-        countryController = new CountryController();
         mockMvc = MockMvcBuilders
                 .standaloneSetup(countryController)
                 .build();
     }
 
+    @Test
+    public void home_ShouldRenderListAllCountries() throws Exception {
+        // Arrange the mock behaviour
+        List<Country> countries = Arrays.asList(
+                new Country.CountryBuilder(1)
+                        .withName("France")
+                        .withCapital("Paris")
+                        .withPopulation(66710000L)
+                        .withFlagFileName("france.png")
+                        .withLanguages(Arrays.asList("French"))
+                        .build(),
+                new Country.CountryBuilder(2)
+                        .withName("Ecuador")
+                        .withCapital("Quito")
+                        .withPopulation(1614400L)
+                        .withFlagFileName("ecuador.png")
+                        .withLanguages(Arrays.asList("Spanish"))
+                        .build()
+        );
+        Mockito.when(countryRepository.findAll()).thenReturn(
+                countries
+        );
+        // Act (perform the MVC request) and Assert results
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(
+                        model().attribute("countries", countries)
+        );
+    }
 }
